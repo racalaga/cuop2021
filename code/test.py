60904927e811039ac2822053f6f85c3e5321f370
lichen <lsmy@kaist.ac.kr>
2021-12-27 17:25:24 +0900

    .

---
+++ code/test.py
@@ -0,0 +1,7 @@
+#Evaluation and optimization of hepatocyte culture media factors by design of experiments (DoE) methodology
+import anndata as ad
+import pandas as pd
+import numpy as np
+import sys 
+test=pd.read_csv("test.csv")
+
